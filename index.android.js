3431c40c0aa0f23db6c66409bf8d96f83ee1ef81
ak110dev <ak.apichan1103@gmail.com>
2016-07-19 18:58:00 +0700

    Chore: initial

---
+++ index.android.js
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  WebView
+} from 'react-native';
+
+class knowbita extends Component {
+  render() {
+    return (
+      <WebView source={{uri: 'https://www.knowbita.cpe.ku.ac.th'}} />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('knowbita', () => knowbita);
